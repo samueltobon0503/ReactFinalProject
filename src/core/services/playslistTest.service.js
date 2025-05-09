62567687ab8f2864e3d5f377fd7c52ac3343ae91
Samuel <toboncs@gmail.com>
2025-05-08 20:25:05 -0500

    Profile mockup applied

--- src/core/services/playslistTest.service.js
+++ src/core/services/playslistTest.service.js
@@ -5,8 +5,6 @@ const BASE_URL = process.env.REACT_APP_URL_BASE;
 
 export const getPlay = () => {
     const token = getHeaders();
-    console.log('Token en getPlay:', token);
-    console.log('URL:', `${BASE_URL}/artists/4Z8W4fKeB5YxbusRsdQVPb`);
     return axios.get(`${BASE_URL}/artists/4Z8W4fKeB5YxbusRsdQVPb`, {
       headers: {
         'Authorization': `Bearer ${token.access_token}`,
